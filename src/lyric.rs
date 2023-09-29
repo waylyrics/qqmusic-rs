82d264c130c507838f00c2e87194332edca8c9a3
poly000 <1348292515@qq.com>
2023-09-29 16:11:52 +0800

    fix: /lyric only accepts songmid

--- src/lyric.rs
+++ src/lyric.rs
@@ -3,14 +3,14 @@ use url::Url;
 use crate::{SongId, QQMusicApi};
 
 pub trait QueryLyric {
-    fn query_lyric(&self, song: SongId<'_>) -> Url;
+    fn query_lyric(&self, song: &str) -> Url;
 }
 
 impl QueryLyric for QQMusicApi {
-    fn query_lyric(&self,  id: SongId<'_>) -> Url {
+    fn query_lyric(&self,  mid: &str) -> Url {
         let mut url = self.base_url.clone();
         url.set_path("/lyric");
-        url.set_query(Some(&id.to_string()));
+        url.set_query(Some(&SongId::Songmid(mid).to_string()));
         url
     }
 }
