a41ddc1d83312bbe49b80eafc5b1461df562de63
poly000 <poly000@users.noreply.github.com>
2024-02-01 08:36:26 +0000

    Automated deployment: Thu Feb  1 08:36:26 UTC 2024 3eeb544d48cffd5b95c9b566e0066070909e7332

--- implementors/core/marker/trait.Copy.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"qqmusic_rs":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"qqmusic_rs/types/enum.SongId.html\" title=\"enum qqmusic_rs::types::SongId\">SongId</a>&lt;'a&gt;"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"qqmusic_rs":["",[["types",[["search",[["search_type",[],["album.rs","artist.rs","lyric.rs","mod.rs","mv.rs","track.rs","track_list.rs"]]]],["user",[],["cookie.rs","get_cookie.rs","set_cookie.rs"]]],["lyric.rs","search.rs","song.rs","user.rs"]]],["lib.rs","lyric.rs","search.rs","song.rs","types.rs","user.rs"]]\
-}');
-createSourceSidebar();
